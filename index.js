3bb6161d5e7b8bd5020f0b51d50adf41417086ea
Furqan Hameed <furqancode@gmail.com>
2023-03-21 22:42:30 +0530

    added crud operations

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(
 );
 
 const db = require("./models");
+const { Member } = require("./models");
 
 db.sequelize
     .sync()
@@ -23,6 +24,40 @@ app.listen(3000, () => {
     console.log("Server listening on port 3000");
 });
 
-app.get("/", (req, res) => {
+app.get("/create", (req, res) => {
+    Member.create({
+        role: "Admin",
+        isAdmin: 1,
+    })
+        .then((res) => {
+            console.log("Record Created ...");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    res.sendStatus(200);
+});
+
+app.get("/select", (req, res) => {
+    Member.findAll({ where: { id: 2 } })
+        .then((result) => {
+            console.log("Records Found ...");
+            return res.json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get("/delete", (req, res) => {
+    Member.destroy({ where: { id: 1 } })
+        .then((result) => {
+            console.log("Records Deleted ...");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
     res.sendStatus(200);
 });
